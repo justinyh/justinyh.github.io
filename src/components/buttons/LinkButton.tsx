3ba45272d3b6fb30434fe97f7e560cb87bc7749b
justinyh <justinyh@usc.edu>
2024-08-24 17:12:21 -0500

    Fix resume link

--- src/components/buttons/LinkButton.tsx
+++ src/components/buttons/LinkButton.tsx
@@ -2,13 +2,6 @@ import { PropsWithChildren, ReactNode } from "react";
 import Link from "../../../node_modules/next/link";
 import { variantClasses } from "./ButtonHelpers";
 
-export const generateButtonEl = (variant: keyof typeof variantClasses, children: ReactNode) => {
-    return (
-        <button tabIndex={-1} className={variantClasses[variant]}>
-            {children}
-        </button>
-    )
-}
 interface LinkButtonProps {
     href: string,
     newTab?: boolean,
@@ -16,16 +9,11 @@ interface LinkButtonProps {
 }
 
 export function LinkButton({ variant="primary", children, href="#", newTab = false }: PropsWithChildren<LinkButtonProps>) {
-    if (newTab) {
         return (
-            <a href={href} target="_blank">
-                {generateButtonEl(variant, children)}
-            </a>
-        )
-    }
-        return (
-        <Link href={href}>
-            {generateButtonEl(variant, children)}
+        <Link href={href} target={newTab ? "_blank" : (void 0)}>
+            <button tabIndex={-1} className={variantClasses[variant]}>
+                {children}
+            </button>
         </Link>
     );
 }

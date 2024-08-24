1df7d35e6704cf3c73d5627b5114eebc9308f3e3
justinyh <justinyh@usc.edu>
2024-08-23 19:27:08 -0500

    Fix base path + use unoptimized images

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ import createMDX from '@next/mdx'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // Configure `pageExtensions` to include markdown and MDX files
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/personal-website-v2",
   output: 'export',
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   experimental: {

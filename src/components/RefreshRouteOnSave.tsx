3be022a27c62e0c69d73ddc8b6d2a1c413cf79f3
Justin Hu <justin.hu764@gmail.com>
2026-08-14 00:28:58 -0500

    Fix cross frame issue

--- src/components/RefreshRouteOnSave.tsx
+++ src/components/RefreshRouteOnSave.tsx
@@ -2,6 +2,9 @@
 import { RefreshRouteOnSave as PayloadLivePreview } from '@payloadcms/live-preview-react'
 import { useRouter } from 'next/navigation.js'
 import React from 'react'
+import config from "@payload-config"
+
+const isProduction = process.env.NEXT_PUBLIC_VERCEL_ENV === 'production';
 
 export const RefreshRouteOnSave: React.FC = () => {
   const router = useRouter()
@@ -9,7 +12,9 @@ export const RefreshRouteOnSave: React.FC = () => {
   return (
     <PayloadLivePreview
       refresh={() => router.refresh()}
-      serverURL={process.env.NEXT_PUBLIC_PAYLOAD_URL ?? ''}
+      serverURL={`https://${isProduction 
+    ? process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL 
+    : process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`}
     />
   )
 }
